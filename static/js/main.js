688c45d8304872270ed4f1d2999e8abc3bbe2d79
Gianluca Mancini <gianlucamancini@outlook.com>
2023-10-03 00:20:51 +0200

    JS iOS fix (veramente)

--- static/js/main.js
+++
@@ -1,6 +0,0 @@
-// Mobile menu
-const mobileMenu = document.getElementById('mobile-menu');
-const mobileMenuButton = document.getElementById('mobile-menu-toggle');
-mobileMenuButton.addEventListener('click', function(event) {
-  mobileMenu.classList.toggle('hidden');
-});
